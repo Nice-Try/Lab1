e0986c039af361b02140744ef1f8e3a2fb8a4003
cmoser96 <cm898946@gmail.com>
2018-10-03 21:42:04 -0400

    moved or and nor together

--- or.v
+++ or.v
@@ -1,19 +1,24 @@
 // Define gate delays
-`define OR or #30 // 2 inputs, and inverter
+`define NOR nor #20 // 2 inputs
+`define XOR xor #20 // 2 inputs
 
 module full32BitOr
 (
   output[31:0] out,
   input[31:0] a,
-  input[31:0] b
+  input[31:0] b,
+  input orflag
 );
   // Generate all the gates
   genvar i;
   generate
     for (i=0; i<32; i=i+1)
     begin:genblock
-      // OR the inputs
-      `OR(out[i], a[i], b[i]);
+      wire _out;
+      // NAND the inputs
+      `NOR(_out, a[i], b[i]);
+      // XOR with andflag: if andflag, out will be AND, otherwise out is NAND
+      `XOR(out[i], _out, orflag);
     end
   endgenerate
 
