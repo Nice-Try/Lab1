94cf276ca88c239b902aba48deadbf327b65b993
Computer Architecture <xue.camille@gmail.com>
2018-10-03 22:07:44 -0400

    Fixed test case labels

--- slt.t.v
+++ slt.t.v
@@ -7,62 +7,62 @@ module testSLT();
   wire carryout, overflow;
   reg dutpassed;
   reg subtract = 1;
-  
+
   full32BitAdder adder (sum, carryout, overflow, a, b, subtract);
   SLT setlessthan(lessthan, sum, overflow);
 
   initial begin
   dutpassed = 1;
 
-  //test cases in the form a_sign | b_sign | overflow   result
+  //test cases in the form a_sign | b_sign | result
 
   a=32'd8; b=32'd3; #2000
   if (lessthan !== 0) begin
     dutpassed = 0;
     $display("Actual:   %b", sum);
-    $display("Failed Test Case 1: ++0 False");
+    $display("Failed Test Case 1: ++ False");
     $display("%b %b 0 %b", a,b,lessthan);
   end
 
   a=32'd2; b=32'd3; #2000
   if (lessthan !== 1) begin
     dutpassed = 0;
-    $display("Failed Test Case 2: ++0 True");
+    $display("Failed Test Case 2: ++ True");
     $display("%b %b 1 %b", a,b,lessthan);
   end
 
   a=32'd5;b=-32'd3; #2000
   if (lessthan !== 0) begin
     dutpassed = 0;
-    $display("Failed Test Case 3: +-1 False");
+    $display("Failed Test Case 3: +- False");
     $display("%b %b 0 %b", a,b,lessthan);
   end
 
   a=32'd0;b=-32'd6;#2000
   if (lessthan !== 0) begin
     dutpassed = 0;
-    $display("Failed Test Case 4: +-0 False");
+    $display("Failed Test Case 4: +- False");
     $display("%b %b 0 %b", a,b,lessthan);
   end
 
   a=-32'd3;b=-32'd8;#2000
   if (lessthan !== 0) begin
     dutpassed = 0;
-    $display("Failed Test Case 5: --0 False");
+    $display("Failed Test Case 5: -- False");
     $display("%b %b 0 %b", a,b,lessthan);
   end
 
   a=-32'd8;b=-32'd3;#2000
   if (lessthan !== 1) begin
     dutpassed = 0;
-    $display("Failed Test Case 6: --0 True");
+    $display("Failed Test Case 6: -- True");
     $display("%b %b 1 %b", a,b,lessthan);
   end
 
   a=-32'd3;b=32'd2;#2000
   if (lessthan !== 1) begin
     dutpassed = 0;
-    $display("Failed Test Case 7: -+0 True");
+    $display("Failed Test Case 7: -+ True");
     $display("%b %b 1 %b", a,b,lessthan);
   end
 
