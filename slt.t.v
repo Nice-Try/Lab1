61b691ca61a007bd7f386a95c8272c008fab21e6
Computer Architecture <xue.camille@gmail.com>
2018-10-03 20:05:09 -0400

    Finished slt test bench

--- slt.t.v
+++ slt.t.v
@@ -1,15 +1,93 @@
 `include "slt.v"
-`timescale 1 ns / ps
+`timescale 1 ns / 1 ps
 
 module testSLT();
   reg[31:0] a, b;
-  reg lessthan;
+  wire lessthan;
   wire[31:0] sum;
   wire carryout, overflow;
+  reg dutpassed;
 
-  SLT setlessthan(lessthan, overflow, a, b);
+  SLT setlessthan(lessthan, a, b);
+  full32BitAdder testadder(sum, carryout, overflow, a, b, 1'b1);
 
   initial begin
+  dutpassed = 1;
+
+  //test cases in the form a_sign b_sign overflow
+
+  a=32'd8; b=32'd3; #1000
+  if (lessthan !== 0) begin
+    dutpassed = 0;
+    $display("Failed Test Case 1: ++0 False");
+    $display("%b %b 0 %b %b", a,b,lessthan, sum);
+  end
+
+  a=32'd2; b=32'd3; #1000
+  if (lessthan !== 1) begin
+    dutpassed = 0;
+    $display("Failed Test Case 1: ++0 True");
+    $display("%b %b 1 %b %b", a,b,lessthan, sum);
+  end
+
+  a=32'd5;b=-32'd3; #1000
+  if (lessthan !== 0) begin
+    dutpassed = 0;
+    $display("Failed Test Case 2: +-1 False");
+    $display("%b %b 0 %b", a,b,lessthan);
+  end
+
+  a=32'd0;b=-32'd6;#1000
+  if (lessthan !== 0) begin
+    dutpassed = 0;
+    $display("Failed Test Case 3: +-0 False");
+    $display("%b %b 0 %b", a,b,lessthan);
+  end
+
+  a=-32'd3;b=-32'd8;#1000
+  if (lessthan !== 0) begin
+    dutpassed = 0;
+    $display("Failed Test Case 4: --0 False");
+    $display("%b %b 0 %b", a,b,lessthan);
+  end
+
+  a=-32'd8;b=-32'd3;#1000
+  if (lessthan !== 1) begin
+    dutpassed = 0;
+    $display("Failed Test Case 4: --0 True");
+    $display("%b %b 1 %b", a,b,lessthan);
+  end
+
+  a=-32'd3;b=32'd2;#1000
+  if (lessthan !== 1) begin
+    dutpassed = 0;
+    $display("Failed Test Case 5: -+0 True");
+    $display("%b %b 1 %b", a,b,lessthan);
+  end
+
+  a=-32'd8;b=32'd3;#1000
+  if (lessthan !== 1) begin
+    dutpassed = 0;
+    $display("Failed Test Case 6: -+1 True");
+    $display("%b %b 1 %b", a,b,lessthan);
+  end
+
+  a=32'd2;b=32'd2;#1000
+  if (lessthan !== 0) begin
+    dutpassed = 0;
+    $display("Failed Test Case 7: same+ False");
+    $display("%b %b 0 %b", a,b,lessthan);
+  end
+
+  a=-32'd4;b=-32'd4;#1000
+  if (lessthan !== 0) begin
+    dutpassed = 0;
+    $display("Failed Test Case 8: same- False");
+    $display("%b %b 0 %b", a,b,lessthan);
+  end
+
+  if(dutpassed==1)
+    $display("All 8 Tests Passed.");
 
   /*Test Cases: a b overflow
   ++T
@@ -19,4 +97,6 @@ module testSLT();
   +-T
   +-F
   -+T
-  -+F
+  -+F */
+  end
+  endmodule
--- slt.v
+++ slt.v
@@ -1,10 +1,9 @@
 `include "adder.v"
-`define XOR xor #20 // 2 inputs
+`define XOR xor #20 // 2 inputs so delay 20
 
 module SLT
 (
   output less,
-  output overflow, //this isn't necessary I just want to see it
   input[31:0] a,
   input[31:0] b
   );
@@ -14,7 +13,7 @@ module SLT
 
   //subtract A-B
   full32BitAdder subtract(.a(a), .b(b), .subtract(1'b1), .overflow(overflow), .sum(sum), .carryout(carryout));
-  `XOR xorgate(sum[31], overflow, less);
+  `XOR xorgate(less, overflow, sum[31]);
   //if negative & no overflow = 1, if overflow & neg = 0, overflow & pos = 1
 
 endmodule
