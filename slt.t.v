1d6b8b89988034e5f1574c642b4ce535373c79ee
Computer Architecture <xue.camille@gmail.com>
2018-10-03 21:58:05 -0400

    working slt and testbench

--- slt.t.v
+++ slt.t.v
@@ -1,93 +1,95 @@
 `include "slt.v"
-`timescale 1 ns / 1 ps
 
 module testSLT();
-  reg[31:0] a, b;
+  reg signed [31:0] a, b;
   wire lessthan;
-  wire[31:0] sum;
+  wire signed [31:0] sum;
   wire carryout, overflow;
   reg dutpassed;
-
-  SLT setlessthan(lessthan, a, b);
-  full32BitAdder testadder(sum, carryout, overflow, a, b, 1'b1);
+  reg subtract = 1;
+  
+  full32BitAdder adder (sum, carryout, overflow, a, b, subtract);
+  SLT setlessthan(lessthan, sum, overflow);
 
   initial begin
   dutpassed = 1;
 
   //test cases in the form a_sign | b_sign | overflow   result
 
-  a=32'd8; b=32'd3; #1000
+  a=32'd8; b=32'd3; #2000
   if (lessthan !== 0) begin
     dutpassed = 0;
+    $display("Actual:   %b", sum);
     $display("Failed Test Case 1: ++0 False");
-    $display("%b %b 0 %b %b", a,b,lessthan, sum);
-  end
-
-  a=32'd2; b=32'd3; #1000
-  if (lessthan !== 1) begin
-    dutpassed = 0;
-    $display("Failed Test Case 1: ++0 True");
-    $display("%b %b 1 %b %b", a,b,lessthan, sum);
-  end
-
-  a=32'd5;b=-32'd3; #1000
-  if (lessthan !== 0) begin
-    dutpassed = 0;
-    $display("Failed Test Case 2: +-1 False");
     $display("%b %b 0 %b", a,b,lessthan);
   end
 
-  a=32'd0;b=-32'd6;#1000
+  a=32'd2; b=32'd3; #2000
+  if (lessthan !== 1) begin
+    dutpassed = 0;
+    $display("Failed Test Case 2: ++0 True");
+    $display("%b %b 1 %b", a,b,lessthan);
+  end
+
+  a=32'd5;b=-32'd3; #2000
   if (lessthan !== 0) begin
     dutpassed = 0;
-    $display("Failed Test Case 3: +-0 False");
+    $display("Failed Test Case 3: +-1 False");
     $display("%b %b 0 %b", a,b,lessthan);
   end
 
-  a=-32'd3;b=-32'd8;#1000
+  a=32'd0;b=-32'd6;#2000
   if (lessthan !== 0) begin
     dutpassed = 0;
-    $display("Failed Test Case 4: --0 False");
+    $display("Failed Test Case 4: +-0 False");
     $display("%b %b 0 %b", a,b,lessthan);
   end
 
-  a=-32'd8;b=-32'd3;#1000
-  if (lessthan !== 1) begin
-    dutpassed = 0;
-    $display("Failed Test Case 4: --0 True");
-    $display("%b %b 1 %b", a,b,lessthan);
-  end
-
-  a=-32'd3;b=32'd2;#1000
-  if (lessthan !== 1) begin
-    dutpassed = 0;
-    $display("Failed Test Case 5: -+0 True");
-    $display("%b %b 1 %b", a,b,lessthan);
-  end
-
-  a=-32'd8;b=32'd3;#1000
-  if (lessthan !== 1) begin
-    dutpassed = 0;
-    $display("Failed Test Case 6: -+1 True");
-    $display("%b %b 1 %b", a,b,lessthan);
-  end
-
-  a=32'd2;b=32'd2;#1000
+  a=-32'd3;b=-32'd8;#2000
   if (lessthan !== 0) begin
     dutpassed = 0;
-    $display("Failed Test Case 7: same+ False");
+    $display("Failed Test Case 5: --0 False");
+    $display("%b %b 0 %b", a,b,lessthan);
+  end
+
+  a=-32'd8;b=-32'd3;#2000
+  if (lessthan !== 1) begin
+    dutpassed = 0;
+    $display("Failed Test Case 6: --0 True");
+    $display("%b %b 1 %b", a,b,lessthan);
+  end
+
+  a=-32'd3;b=32'd2;#2000
+  if (lessthan !== 1) begin
+    dutpassed = 0;
+    $display("Failed Test Case 7: -+0 True");
+    $display("%b %b 1 %b", a,b,lessthan);
+  end
+
+  a=-32'd8;b=32'd3;#2000
+  if (lessthan !== 1) begin
+    dutpassed = 0;
+    $display("Failed Test Case 8: -+1 True");
+    $display("%b %b 1 %b", a,b,lessthan);
+  end
+
+  a=32'd2;b=32'd2;#2000
+  if (lessthan !== 0) begin
+    dutpassed = 0;
+    $display("Failed Test Case 9: same+ False");
     $display("%b %b 0 %b", a,b,lessthan);
   end
 
   a=-32'd4;b=-32'd4;#1000
   if (lessthan !== 0) begin
     dutpassed = 0;
-    $display("Failed Test Case 8: same- False");
-    $display("%b %b 0 %b", a,b,lessthan);
+    $display("Failed Test Case 10: same- False");
+    $display("%b %b 0 %b %b", a,b,lessthan, overflow);
   end
 
   if(dutpassed==1)
     $display("All 8 Tests Passed.");
 
   end
+
   endmodule
--- slt.v
+++ slt.v
@@ -4,16 +4,11 @@
 module SLT
 (
   output less,
-  input[31:0] a,
-  input[31:0] b
+  input signed [31:0] sum,
+  input overflow
   );
 
   wire overflow, carryout;
-  wire[31:0] sum;
-
-  //subtract A-B
-  full32BitAdder subtract(.a(a), .b(b), .subtract(1'b1), .overflow(overflow), .sum(sum), .carryout(carryout));
-  `XOR xorgate(less, overflow, sum[31]);
-  //if negative & no overflow = 1, if overflow & neg = 0, overflow & pos = 1
+  and andgate(less, 1'b1, sum[31]); //output is just sign of sum[31]
 
 endmodule
