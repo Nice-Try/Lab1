3e22b559f85441c18818e0779a37fe6d8e283b62
Computer Architecture <xue.camille@gmail.com>
2018-10-04 18:48:08 -0400

    ALU skeleton

--- alu.v
+++ alu.v
@@ -1,3 +1,38 @@
+module ALUcontrolLUT
+(
+output reg[2:0] 	muxindex,
+output reg	othercontrolsignal
+input[2:0]	ALUcommand
+)
+
+  always @(ALUcommand) begin
+    case (ALUcommand)
+      `ADD:  begin muxindex = 0; othercontrolsignal = 0; end
+      `SUB:  begin muxindex = 0; othercontrolsignal = 1; end
+      `XOR:  begin muxindex = 1; othercontrolsignal = 0; end
+      `SLT:  begin muxindex = 2; othercontrolsignal = 0; end
+      `AND:  begin muxindex = 3; othercontrolsignal = 0; end
+      `NAND: begin muxindex = 3; othercontrolsignal = 1; end
+      `NOR:  begin muxindex = 4; othercontrolsignal = 1; end
+      `OR:   begin muxindex = 4; othercontrolsignal = 0; end
+    endcase
+  end
+endmodule
+
+module MUX
+(
+  output result,
+  output carryout,
+  output zero,
+  output overflow,
+  input[2:0] muxindex,
+  input othercontrolsignal,
+  input[31:0] a,
+  input[31:0] b
+  );
+  // TODO: Make the MUX
+endmodule
+
 module ALU
 (
 output[31:0]  result,
@@ -8,5 +43,8 @@ input[31:0]   operandA,
 input[31:0]   operandB,
 input[2:0]    command
 );
-	// Your code here
+
+	ALUcontrolLUT lut (muxindex, othercontrolsignal, command);
+  MUX mux1 (result, carryout, zero, overflow, muxindex, othecontrolsignal, a, b;
+
 endmodule
